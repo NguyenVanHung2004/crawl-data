83efcfc0b5ace14228d8572f09dbe3a707417253
NguyenVanHung2004 <22028118@vnu.edu.vn>
2026-04-17 15:53:51 +0700

    crawl page  6 - 16

--- main_pipeline.py
+++ main_pipeline.py
@@ -13,7 +13,7 @@ VTV_CATEGORIES = [
     "chinh-tri", "xa-hoi", "phap-luat", "the-gioi", 
     "kinh-te", "y-te", "doi-song", "cong-nghe", "giao-duc"
 ]
-START_PAGE = 1
+START_PAGE = 6
 NUM_PAGES = 1  
 RAW_BASE_DIR = "data/raw"
 DATASET_BASE_DIR = "data/dataset"
